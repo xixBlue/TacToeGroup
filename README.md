cf0227dd1f7132409c0179f79216feb58b32c702
AUJuan Villaman <AUJ.Villaman@autolus.com>
2020-07-11 16:13:41 -0400

    Co-authored-by: danego <nevesdane@gmail.com> Co-authored-by: csaisem@yahoo.com <csaisem@yahoo.com> Co-authored-by: eddygutierrez175@gmail.com <eddygutierrez175@gmail.com> Co-authored-by: jasineath@outlook.com <jasineath@outlook.com>

--- README.md
+++ README.md
@@ -1,32 +1,42 @@
 TICTOCTOE PAIR CODING
 
 Play Loop:
-1. Player Cycle through clicks
+1. Player clicks
 2. If Cell has object, block cell
 3. Check if state is in any of 8 Winning States after each move
-4. 
-
+  A) Alert a Win
+  B) Or continue play loop
+  C) Or alert draw
 
 General To-Do:
 1. Store state of board internally
 2. Update w/ each turn
-3. Reset internal after win ... (w/ button)
-
-git init
-git status 
-git clone <url>.
-git status
-git add -A
-git commit -m"first repo"
-git pull origin master
-git push origin master
+3. Reset internal after win ... (w/ reset button)
 
 
+JavaScript Internal Summary: 
+- createBoard()
+  - sets HTML cells' text back to empty
+  - creates new 2D array for internal JS board representaion
 
-1. Re-Structure (proofread) Code
-2. make board look prettier/flashier - CSS & HTML
-  - sidebar (instructions - tic tac toe or specific to program)
-  - make defualt banners better
-  - add reset to banners
-  - winner winner - apply banner colors for winner
-  - (both) players clicking same spot skips over player 
+- makeTurn()
+  - checks if didIWin() 
+    - fills all empty squares
+    - alerts players
+  - or if game was draw (alerts player)
+
+- didIWin() [Juan's favorite!]
+  - pass array and player type
+  - check for three in a row for columns, rows, & diagonals
+  - return immediately if three in a row of same kind are found
+
+- window.onload()
+  - click a square
+    - sets banners
+    - calls makeTurn and passes in player (based on current turnCount)
+  - reset button
+   - calls createBoard() to replace board
+   - sets turnCount to 0
+   - sets player banners styling back to default
+
+  
--- tictactoe.js
+++ tictactoe.js
@@ -1,100 +1,95 @@
 //Tic Tac Toe Dawgs
 let turnCount = 0;
+let boardArray = createTheBoard();
 const dashboard = document.getElementById('dashboard');
 const reset = document.getElementById('reset');
 
-  window.onload = function(){
-    //adding event listener to dashboard
-    dashboard.addEventListener("click", function(e){
-        //splitting the dashboard cordinates to array
-        let coordinatesString = e.target.id;
-        let coordStringArray = coordinatesString.split('');
-        //assigning the coordinates to columns and rows for later piping to makeTurn()
-        let columnValue = coordStringArray[1];
-        let rowValue = coordStringArray[2];
-        const playerX = 'X';
-        const playerO = 'O';
-        //incrementing turnCount as turnCount distiguishes players
-        turnCount++;
-        
-        let playerBannerX = document.getElementById('playerX');
-        let playerBannerO = document.getElementById('playerO');
 
-        //even turn numbers & 0 is Player X
-        if (turnCount % 2 === 1 && e.target.innerText === '') {
-          //check for already clicked square
-          //if makeTurn returns false
-          if (makeTurn(columnValue, rowValue, playerX)) {
-            e.target.innerText = playerX;
-            playerBannerX = document.getElementById('playerX');
-            if (!playerBannerX.classList.contains("playerX")) {
-              playerBannerX.classList.toggle("playerX");
-            }
-            if (playerBannerO.classList.contains("playerO")) {
-              playerBannerO.classList.toggle("playerO");
-            }
-          } 
-        }
-        //odd turn numbers is Player O
-        else if (turnCount % 2 === 0 && e.target.innerText === ''){
-          //check for already clicked square
-          //if makeTurn returns false
-          if (makeTurn(columnValue, rowValue, playerO)) {
-            e.target.innerText = playerO;
-            playerBannerO = document.getElementById('playerO');
-            if (!playerBannerO.classList.contains("playerO")) {
-              playerBannerO.classList.toggle("playerO");
-            }
-            if (playerBannerX.classList.contains("playerX")) {
-              playerBannerX.classList.toggle("playerX");
-            }
+window.onload = function() {
+  
+  let playerBannerX = document.getElementById('playerX');
+  let playerBannerO = document.getElementById('playerO');
+  //adding event listener to dashboard
+  dashboard.addEventListener("click", function(e){
+      //splitting the dashboard cordinates to array
+      let coordinatesString = e.target.id;
+      let coordStringArray = coordinatesString.split('');
+      //assigning the coordinates to columns and rows for later piping to makeTurn()
+      let columnValue = coordStringArray[1];
+      let rowValue = coordStringArray[2];
+      const playerX = 'X';
+      const playerO = 'O';  
+
+      //even turn numbers & turn0 is Player X
+      if (turnCount % 2 === 0 && e.target.innerText === '') {
+        //check for already clicked square
+        //if makeTurn returns false
+        if (makeTurn(columnValue, rowValue, playerX)) {
+          e.target.innerText = playerX;
+          playerBannerX = document.getElementById('playerX');
+          if (!playerBannerX.classList.contains("playerX")) {
+            playerBannerX.classList.toggle("playerX");
           }
-          /*
+          if (playerBannerO.classList.contains("playerO")) {
+            playerBannerO.classList.toggle("playerO");
+          }
+        } 
+      }
+      //odd turn numbers is Player O
+      else if (turnCount % 2 === 1 && e.target.innerText === ''){
+        //check for already clicked square
+        //if makeTurn returns false
+        if (makeTurn(columnValue, rowValue, playerO)) {
           e.target.innerText = playerO;
-          makeTurn(columnValue, rowValue, playerO);
           playerBannerO = document.getElementById('playerO');
           if (!playerBannerO.classList.contains("playerO")) {
             playerBannerO.classList.toggle("playerO");
           }
-          */
-          //add check for already clicked square
-          //check internal JS array using rows and cols to see if a mark's already there
-        } 
-        
-        else {
-          console.log('Error');
+          if (playerBannerX.classList.contains("playerX")) {
+            playerBannerX.classList.toggle("playerX");
+          }
         }
-    })
-    reset.addEventListener("click", function(e){
-      //alert('Game Draw!');
-      boardArray = createTheBoard();
-      console.log('Reset Clicked')
-    });
-  }
+      } 
+      else {
+        console.log('Error');
+      }
+  })
+  reset.addEventListener("click", function(e){
+  
+    boardArray = createTheBoard();
+    console.log('Reset Clicked')
+
+    if (playerBannerX.classList.contains("playerX")) {
+      playerBannerX.classList.toggle("playerX");
+    }
+    if (playerBannerO.classList.contains("playerO")) {
+      playerBannerO.classList.toggle("playerO");
+    }
+  });
+}
 
 //2d array - each sub-array is a row
 function createTheBoard() {
 
-    //empty HTML representation
-    //dashboard.children
-    for(let j = 0; j < 3; j++){
-      for (let i = 0; i < 3; i++) {
+  //empty HTML representation
+  //dashboard.children
+  for(let j = 0; j < 3; j++){
+    for (let i = 0; i < 3; i++) {
 
-        const boardCell = document.getElementById('c' + j + i);
-        boardCell.innerText = '';
-      }
+      const boardCell = document.getElementById('c' + j + i);
+      boardCell.innerText = '';
     }
-
-    //reset turnCount (back to 0)
-    turnCount = 0;
-    
-    //create board 
-    const boardArray = new Array(3);
-    //Create each row and fill
-    for (let i = 0; i < boardArray.length; i++){
-        boardArray[i] = new Array(3).fill('-');
-    }
-    return boardArray;
+  }
+  //reset turnCount (back to 0)
+  turnCount = 0;
+  
+  //create board 
+  const boardArray = new Array(3);
+  //Create each row and fill
+  for (let i = 0; i < boardArray.length; i++){
+      boardArray[i] = new Array(3).fill('-');
+  }
+  return boardArray;
 }
 
 const didIWin = (arr, player) => {
@@ -117,49 +112,33 @@ const didIWin = (arr, player) => {
   return false;
 }
 
-function makeTurn(row, column, player) {
+const makeTurn = (row, column, player) => {
   //if given cell is empty
   if (boardArray[row][column] === '-') {
-    if (turnCount === 9) {
-      for(let j = 0; j < 3; j++){
-        for (let i = 0; i < 3; i++) {
-          //check if board cell is empty
-          if (boardArray[row][column] === '-') {
-            //continue game
-          }
-        }
-      }
-      alert("You're both winners!");
-    }
+  
     boardArray[row][column] = player;
+    //incrementing turnCount as turnCount distiguishes players
+    turnCount++;
     if (didIWin(boardArray, player)) {
 
       console.log('Winner winner chicken dinner: Player ' + player);
       alert('Winner winner chicken dinner: Player ' + player);
-      //call some function to reset board
-      //boardArray = createTheBoard();
+      
+      //fill the board w/ random marks
+      for(let j = 0; j < 3; j++){
+        for (let i = 0; i < 3; i++) {
+          //check if board cell is empty
+          if (boardArray[i][j] === '-') {
+            //fill w/ marks
+            boardArray[i][j] = 'Z';
+          }
+        }
+      }
+    }
+    else if (turnCount === 9) {
+      alert("Draw! You're both winners!");
     } 
-    //alert('You're both winners, it's a draw)
-    // else {
-    //   for(let j = 0; j < 3; j++){
-    //     for (let i = 0; i < 3; i++) {
-    //       //check if board cell is empty
-    //       if (boardArray[row][column] === '-') {
-    //         //continue game
-    //       }
-    //     }
-    //   }
-    //   alert("You're both winners!");
-    // }
     return true;
-  }
-  else {
-    console.log('No winner yet.')
-    return false;
   }
 }
 
-
-//JS Console Testing Area (ie visually ugly)
-let boardArray = createTheBoard();
-//console.log(boardArray);
